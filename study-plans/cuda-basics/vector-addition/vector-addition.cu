ce0680ab0a64db95f1d0571f6c2c3381e8e3f268
Aman Singh <7347amansingh@gmail.com>
2026-08-22 17:06:06 +0530

    Solve vector-addition

---
+++ study-plans/cuda-basics/vector-addition/vector-addition.cu
@@ -0,0 +1,17 @@
+#include <cuda_runtime.h>
+
+__global__ void vector_add(const float* A, const float* B, float* C, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for(int i = idx; i < N; i += stride){
+            C[i] = A[i] + B[i];
+    }
+}
+
+extern "C" void solve(const float* A, const float* B, float* C, int N) {
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+    vector_add<<<blocks, threads>>>(A, B, C, N);
+    cudaDeviceSynchronize();
+}
